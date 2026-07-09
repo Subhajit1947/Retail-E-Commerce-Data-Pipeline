83d8a83be10f4ce4d2a03213775ff73c8340286c
Subhajit Das <dassubhajit@gmail.com>
2026-07-09 11:13:56 +0530

    write task for order script upload to s3 in dag

--- airflow/dags/main_pipeline.py
+++ airflow/dags/main_pipeline.py
@@ -2,6 +2,7 @@ from airflow.sdk import DAG
 from airflow.providers.standard.operators.python import PythonOperator
 from airflow.providers.standard.operators.bash import BashOperator
 from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.hooks.S3_hook import S3Hook
 from datetime import datetime
 
 from include.utils.helper import run_daily_pipeline
@@ -11,6 +12,11 @@ import pandas as pd
 s3_bucket=os.environ["S3_BUCKET"]
 aws_key=os.environ["AWS_ACCESS_KEY"]
 aws_secret=os.environ["AWS_SECRET_KEY"]
+AIRFLOW_HOME = os.environ.get('AIRFLOW_HOME','/opt/airflow')
+def upload_to_s3(filename, key):
+    hook = S3Hook()
+    hook.load_file(filename=filename, key=key, bucket_name=s3_bucket, replace=True)
+
 
 
 dag=DAG(
@@ -31,5 +37,12 @@ task=PythonOperator(
     dag=dag
 )
 
-
+order_script_upload_task = PythonOperator(
+    task_id= 'Cust_Script_To_S3',
+    python_callable= upload_to_s3,
+    op_kwargs=dict(
+        filename = AIRFLOW_HOME+"/dags/include/silver_scripts/order_tranformation.py", 
+        key = "Scripts/order_tranformation.py"
+    )
+)
 
