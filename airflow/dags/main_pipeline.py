0b86d07cf3004c1d990e69e9740e612aa9e0341f
Subhajit Das <dassubhajit@gmail.com>
2026-07-10 12:02:36 +0530

    add batch name

--- airflow/dags/main_pipeline.py
+++ airflow/dags/main_pipeline.py
@@ -150,12 +150,13 @@ is_emr_cluster_created=EmrJobFlowSensor(
     aws_conn_id="aws_default"
 )
 customer_silver_job  = EmrAddStepsOperator(
-        task_id="Submitting_Spark_Job_customer",
-        job_flow_id="{{ task_instance.xcom_pull(task_ids='Create_EMR_Cluster', key='return_value') }}",
-        aws_conn_id="aws_default",
-        steps=SPARK_STEPS,
-        params={
-            "BUCKET_NAME": s3_bucket,
-            "SCRIPT_KEY": "Scripts/customer_transformation.py"
-        },
-    )
+    task_id="Submitting_Spark_Job_customer",
+    job_flow_id="{{ task_instance.xcom_pull(task_ids='Create_EMR_Cluster', key='return_value') }}",
+    aws_conn_id="aws_default",
+    steps=SPARK_STEPS,
+    params={
+        "BUCKET_NAME": s3_bucket,
+        "SCRIPT_KEY": "Scripts/customer_transformation.py",
+        "BATCH_NAME":"Customer Silver Batch"
+    },
+)
