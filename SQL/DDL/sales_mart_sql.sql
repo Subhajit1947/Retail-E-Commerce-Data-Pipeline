e061b6fe271d3ef1a2ea979ceed50b0180d084c0
Subhajit Das <dassubhajit@gmail.com>
2026-07-22 10:32:52 +0530

    Create Sales Analytics Materialized View

---
+++ SQL/DDL/sales_mart_sql.sql
@@ -0,0 +1,25 @@
+CREATE MATERIALIZED VIEW sales.mv_sales_mark AS
+SELECT
+    fo.order_id,
+    fo.order_date,
+    fo.payment_method,
+    fo.order_platform,
+    fo.order_month,
+    fo.order_year,
+    fod.order_details_id,
+    fod.product_quantity,
+    dp.product_name,
+    dp.brand_name,
+    dp.product_category,
+    (dp.product_price * fod.product_quantity) AS total_revenue,
+    dc.customer_id,
+    dc.cust_city,
+    dc.cust_country
+FROM
+    sales.fact_orders fo
+    JOIN sales.fact_order_details fod ON fo.order_id=fod.order_id
+    JOIN sales.dim_product dp ON fod.product_id=dp.product_id
+    JOIN sales.dim_customer dc ON fo.order_customer_id=dc.customer_id
+
+
+
